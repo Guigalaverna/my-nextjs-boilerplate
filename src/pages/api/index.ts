e0d30163462b496a080650f9e00256d2f2fd9f4e
Guigalaverna <guigalaverna@gmail.com>
2021-08-13 23:04:23 -0300

    feat: create next.js structure

---
+++ src/pages/api/index.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return res.json({ status: req.statusCode, message: `Status code: ${req.statusCode}`})
+}
